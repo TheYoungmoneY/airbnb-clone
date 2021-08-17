5c24dab551e63c12964a1da3c21e46cbe6f6b26a
Youngmi <youngmi9347@gmail.com>
2021-08-17 22:05:53 +0900

    ~11.6

--- rooms/views.py
+++ rooms/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from math import ceil
+from django.shortcuts import redirect, render
+from django.core.paginator import EmptyPage, Paginator
 from . import models
 
 # Create your views here.
 def all_rooms(request):
-    page = int(request.GET.get("page", 1))
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    return render(request, "rooms/home.html", context={"rooms": all_rooms})
+    page = request.GET.get("page", 1)
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        pages = paginator.page(int(page))
+        return render(
+            request,
+            "rooms/home.html",
+            {
+                "pages": pages,
+            },
+        )
+    except EmptyPage:
+        return redirect("/")
