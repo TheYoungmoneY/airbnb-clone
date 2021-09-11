a6cf549a020710e9f4eb90b40d6c2968ea9bdcfc
Youngmi <youngmi9347@gmail.com>
2021-09-12 00:21:45 +0900

    ~20.10

--- config/settings.py
+++ config/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/3.2/topics/auth/customizing/
 AUTH_USER_MODEL = "users.User"
 
